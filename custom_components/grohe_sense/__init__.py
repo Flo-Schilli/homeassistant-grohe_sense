30ba13f99f14ed3e27bb800c6929e4b84219e854
Flo-Schilli <github@flo.schilli.dev>
2024-12-05 20:46:25 +0100

    Added dashboard export action BE AWARE: There is a warning that something cannot be parsed but it is working so far

--- custom_components/grohe_sense/__init__.py
+++ custom_components/grohe_sense/__init__.py
@@ -2,7 +2,7 @@ import logging
 from typing import List
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, ServiceCall, ServiceResponse, SupportsResponse
 from homeassistant.helpers import aiohttp_client
 from custom_components.grohe_sense.api.ondus_api import OndusApi
 from custom_components.grohe_sense.const import DOMAIN, CONF_USERNAME, CONF_PASSWORD, CONF_PLATFORM
@@ -11,14 +11,13 @@ from custom_components.grohe_sense.enum.ondus_types import GroheTypes
 
 _LOGGER = logging.getLogger(__name__)
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     _LOGGER.debug("Loading Grohe Sense")
 
     session = aiohttp_client.async_get_clientsession(hass)
 
     api = OndusApi(session)
-    state = await api.login(entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD])
+    await api.login(entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD])
 
     devices: List[GroheDevice] = await GroheDevice.get_devices(api)
 
@@ -26,4 +25,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, CONF_PLATFORM)
 
-    return state
+    async def handle_export(call: ServiceCall) -> ServiceResponse:
+        _LOGGER.debug('Export data for params: %s', call.data)
+        dashboard = await api.get_dashboard()
+        return dashboard.to_dict()
+
+    hass.services.async_register(DOMAIN, 'getDashboard', handle_export, schema=None, supports_response=SupportsResponse.ONLY)
+
+    return True
